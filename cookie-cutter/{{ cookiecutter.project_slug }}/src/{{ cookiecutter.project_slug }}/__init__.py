45434913db98d6648a61819b9984c76124057909
Matthias Mair <code@mjmair.com>
2023-01-04 13:25:53 +0100

    fix formatting

--- cookie-cutter/{{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}/__init__.py
+++ cookie-cutter/{{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}/__init__.py
@@ -3,5 +3,5 @@
 from .{{ cookiecutter.project_plugin_name }} import {{ cookiecutter.project_plugin_name }}
 
 __all__ = [{{ cookiecutter.project_plugin_name }}, ]
-__author__ = """{{ cookiecutter.full_name }}"""
-__email__ = '{{ cookiecutter.email }}'
+__author__ = "{{ cookiecutter.full_name }}"
+__email__ = "{{ cookiecutter.email }}"
