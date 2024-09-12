2291398301fc64c1c0cdb071acc52a7e67b71e4d
Matthias Mair <code@mjmair.com>
2024-09-12 19:50:02 +0200

    __all__ values should be strings

--- {{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}/__init__.py
+++ {{ cookiecutter.project_slug }}/src/{{ cookiecutter.project_slug }}/__init__.py
@@ -2,6 +2,6 @@
 
 from .{{ cookiecutter.project_plugin_name }} import {{ cookiecutter.project_plugin_name }}
 
-__all__ = [{{ cookiecutter.project_plugin_name }}, ]
+__all__ = ['{{ cookiecutter.project_plugin_name }}', ]
 __author__ = "{{ cookiecutter.full_name }}"
 __email__ = "{{ cookiecutter.email }}"
